cfb946618fa4c24405bdd4778c60b7f6c0849517
UlugbekYusupov <bilmadim.uz@gmail.com>
2026-03-08 23:56:30 +0500

    Manager page bug fixes in table order/close

--- backend/src/modules/categories/categories.routes.ts
+++ backend/src/modules/categories/categories.routes.ts
@@ -2,7 +2,10 @@ import { Router } from "express";
 import { activeBranchMiddleware } from "../../middlewares/activeBranch.js";
 import { authMiddleware } from "../../middlewares/auth.js";
 import { branchScopeMiddleware } from "../../middlewares/branchScope.js";
-import { requirePermissions } from "../../middlewares/permissions.js";
+import {
+  requireManagerAnyPermissions,
+  requirePermissions
+} from "../../middlewares/permissions.js";
 import { requireRoles } from "../../middlewares/roles.js";
 import { categoriesController } from "./categories.controller.js";
 
@@ -15,10 +18,10 @@ categoriesRouter.use(
   branchScopeMiddleware
 );
 
-categoriesRouter.get("/", requirePermissions(["PRODUCTS_VIEW"]), (req, res) =>
+categoriesRouter.get("/", requireManagerAnyPermissions(["PRODUCTS_VIEW", "ORDERS_MANAGE"]), (req, res) =>
   categoriesController.list(req, res)
 );
-categoriesRouter.get("/:categoryId", requirePermissions(["PRODUCTS_VIEW"]), (req, res) =>
+categoriesRouter.get("/:categoryId", requireManagerAnyPermissions(["PRODUCTS_VIEW", "ORDERS_MANAGE"]), (req, res) =>
   categoriesController.getById(req, res)
 );
 categoriesRouter.post("/", requirePermissions(["PRODUCTS_MANAGE"]), (req, res) =>
