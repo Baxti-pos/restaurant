76a24b59f54de376d4de4d8ef1ace7112a81f9e5
UlugbekYusupov <bilmadim.uz@gmail.com>
2026-03-25 23:40:15 +0500

    chore: stop tracking compiled dist output

--- backend/dist/src/modules/auth/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { authMiddleware } from "../../middlewares/auth.js";
-import { requireRoles } from "../../middlewares/roles.js";
-import { authController } from "./auth.controller.js";
-export const authRouter = Router();
-authRouter.post("/login", (req, res) => authController.login(req, res));
-authRouter.post("/select-branch", authMiddleware, requireRoles(["OWNER", "MANAGER"]), (req, res) => authController.selectBranch(req, res));
-authRouter.get("/me", authMiddleware, requireRoles(["OWNER", "MANAGER"]), (req, res) => authController.me(req, res));
-authRouter.patch("/me", authMiddleware, requireRoles(["OWNER", "MANAGER"]), (req, res) => authController.updateMe(req, res));
